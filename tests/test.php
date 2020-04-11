631dc230673998053126d9429ef2d1a08d1abe96
Dante <35383529+srdante@users.noreply.github.com>
2020-04-11 17:04:53 -0300

    Add files via upload

---
+++ tests/test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use SrDante\EthereumUtils\EthCore;
+
+var_dump(EthCore::generateData('0x387641822Af08ea9028bC9055760cDc5cA00cC0b', 1, 18));
